a3e6124e6d9fc0d25ee292a2dfb72d182daff106
Anusha Ragula <ragulaanushaanu@gmail.com>
2026-06-17 20:01:16 +0530

    Updated database config with dotenv

--- database.py
+++ database.py
@@ -1,7 +1,14 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:Anusha%401629@localhost/Bank"
+# load .env file
+load_dotenv()
+
+# get database URL from .env
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
